f2d634232691f2730a08f1ee8140d24ea29e2077
2811458016 <2811458016@qq.com>
2026-09-13 18:55:01 +0800

    test: extend v5 upgrade fixture rollback to migration 12 search tables

--- tests/test_agent_messages.py
+++ tests/test_agent_messages.py
@@ -170,6 +170,8 @@ def test_existing_version_five_database_upgrades_without_checksum_changes(tmp_pa
         connection.execute("DROP TABLE IF EXISTS tool_budget_reservations")
         connection.execute("DROP TABLE IF EXISTS tool_budget_usage")
         connection.execute("DROP TABLE IF EXISTS step_checkpoints")
+        connection.execute("DROP TABLE IF EXISTS search_sequence")
+        connection.execute("DROP TABLE IF EXISTS search_index")
         connection.execute("ALTER TABLE runs DROP COLUMN archived_at")
         connection.execute("ALTER TABLE runs DROP COLUMN deleted_at")
         connection.execute("PRAGMA user_version = 5")
@@ -179,4 +181,4 @@ def test_existing_version_five_database_upgrades_without_checksum_changes(tmp_pa
     assert upgraded.migration_records()[:5] == original
     assert upgraded.migration_records()[5].name == "v03_agent_messages"
     assert upgraded.migration_records()[6].name == "v03_hierarchical_memories"
-    assert upgraded.migration_records()[-1].name == "v03_p6_step_checkpoints"
+    assert upgraded.migration_records()[-1].name == "v03_p7_a1_global_search_fts"
